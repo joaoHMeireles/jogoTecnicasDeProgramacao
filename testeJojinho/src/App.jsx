2cf9ba7e3deb236940e0eb394c9e8fd6e27535a7
Nihilth <91199065+NIhilth@users.noreply.github.com>
2022-07-31 10:03:24 -0300

    fases

--- testeJojinho/src/App.jsx
+++ testeJojinho/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.scss'
+import floresta from "./assets/background.png"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import PaginaInicial from './PaginaInicial/PaginaInicial'
 import Fase from './Fases/Fase'
@@ -6,42 +7,149 @@ import Ranking from './Ranking/Ranking'
 
 const habilidadesHeroi1 = [
   {
-      id: 1,
-      nome: "FLAUTADA",
-      dano: 3,
-      mana: 0
+    id: 1,
+    nome: "FLAUTADA",
+    dano: 3,
+    mana: 0
   },
   {
-      id: 2,
-      nome: "Som estridente",
-      dano: 6,
-      mana: 2
+    id: 2,
+    nome: "Som estridente",
+    dano: 6,
+    mana: 1
   }
 ]
 
-const habilidadesRatoBebe = [
+const habilidadesHeroi2 = [
   {
+    id: 1,
+    nome: "FLAUTADA",
+    dano: 3,
+    mana: 0
+  },
+  {
+    id: 2,
+    nome: "Som estridente",
+    dano: 6,
+    mana: 1
+  },
+  {
+    id: 3,
+    nome: "Melodia do inferno",
+    dano: 9,
+    mana: 2
+  },
+  {
+    id: 4,
+    nome: "Curar",
+    dano: 2,
+    mana: 3
+  }
+]
+
+const ratoBebe = {
+  nome: "Rato Bebê",
+  vida: 8,
+  habilidades: [
+    {
       id: 1,
       nome: "Arranhar",
       dano: 2,
       mana: 0
-  }
-]
+    }
+  ]
+}
+
+const ratao = {
+  nome: "Rato Atroz",
+  vida: 14,
+  habilidades: [
+    {
+      id: 1,
+      nome: "Arranhar",
+      dano: 2,
+      mana: 0
+    },
+    {
+      id: 2,
+      nome: "Morder",
+      dano: 5,
+      mana: 0
+    },
+  ]
+}
+
+const rataoDeArmadura = {
+  nome: "Rato Soldado",
+  vida: 18,
+  habilidades: [
+    {
+      id: 1,
+      nome: "Arranhar",
+      dano: 2,
+      mana: 0
+    },
+    {
+      id: 2,
+      nome: "Morder",
+      dano: 5,
+      mana: 0
+    },
+    {
+      id: 3,
+      nome: "Investida",
+      dano: 7,
+      mana: 0
+    }
+  ]
+}
+
+const rataoRei = {
+  nome: "Rato Rei",
+  vida: 18,
+  habilidades: [
+    {
+      id: 1,
+      nome: "Arranhar",
+      dano: 2,
+      mana: 0
+    },
+    {
+      id: 2,
+      nome: "Morder",
+      dano: 5,
+      mana: 0
+    },
+    {
+      id: 3,
+      nome: "Investida",
+      dano: 7,
+      mana: 0
+    },
+    {
+      id: 4,
+      nome: "Enforcamento",
+      dano: 9,
+      mana: 0
+    }
+  ]
+}
 
 function App() {
   return (
     <div className="App">
       <div className='Jojinho'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<PaginaInicial />}/>
-          <Route path='/fase/1/1' element={<Fase nomeInimigo = "Rato Bebê" habilidadesVilao={habilidadesRatoBebe} habilidadesHeroi={habilidadesHeroi1} imagem={1}/>}/>
-          <Route path='/fase/1/2' element={<Fase />}/>
-          <Route path='/fase/2/1' element={<Fase />}/>
-          <Route path='/fase/2/2' element={<Fase />}/>
-          <Route path='/ranking' element={<Ranking />}/>
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<PaginaInicial />} />
+            <Route path='/fase/1/1' element={<Fase inimigo={ratoBebe} habilidadesHeroi={habilidadesHeroi1} imagem={floresta} fase={1} proximaFase="/fase/1/2"/>} />
+            <Route path='/fase/1/2' element={<Fase inimigo={ratao} habilidadesHeroi={habilidadesHeroi1} imagem={floresta} fase={1} proximaFase="/fase/2/1"/>} />
+            <Route path='/fase/2/1' element={<Fase inimigo={rataoDeArmadura} habilidadesHeroi={habilidadesHeroi2} imagem={"cidade"} fase={2} proximaFase="/fase/2/2"/>} />
+            <Route path='/fase/2/2' element={<Fase inimigo={rataoRei} habilidadesHeroi={habilidadesHeroi2} imagem={"cidade"} fase={2} proximaFase="/acabou"/>} />
+            <Route path='/ranking' element={<Ranking />} />
+            <Route path='/acabou' element={<Ranking />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </div>
   )
--- testeJojinho/src/Fases/Fase.jsx
+++ testeJojinho/src/Fases/Fase.jsx
@@ -3,55 +3,71 @@ import { Link } from 'react-router-dom'
 import './Fase.scss'
 
 function Fase(props) {
-    const nomeInimigo = props.nomeInimigo
-    const habilidadesVilao = props.habilidadesVilao
+    const rato = props.inimigo
+    let vida, mana
+    if(props.fase == 1){
+        vida = 15
+        mana = 4
+    } else if (props.fase == 2){
+        vida = 20
+        mana = 6
+    }
+    const nomeInimigo = rato.nome
+    const habilidadesVilao = rato.habilidades
     const habilidadesHeroi = props.habilidadesHeroi
-    const [vidaHeroi, setVidaHeroi] = useState(15)
-    const [manaHeroi, setManaHeroi] = useState(6)
-    const [vidaInimigo, setVidaInimigo] = useState(8)
+    const [vidaHeroi, setVidaHeroi] = useState(vida)
+    const [manaHeroi, setManaHeroi] = useState(mana)
+    const [vidaInimigo, setVidaInimigo] = useState(rato.vida)
+    const [venceu, setVenceu] = useState(0)
 
     const acoes = habilidadesHeroi.map((habilidade) => {
-        return(
+        return (
             <button key={habilidade.id} onClick={turno} dano={habilidade.dano} mana={habilidade.mana}>{habilidade.nome}</button>
         )
     })
 
     function acaoHeroi(dano, mana) {
         setVidaInimigo(vidaInimigo - dano)
-        setManaHeroi(manaHeroi - mana)
+        if (mana == 0 && manaHeroi < 6) {
+            setManaHeroi(manaHeroi + 1)
+        } else {
+            setManaHeroi(manaHeroi - mana)
+        }
     }
 
-    function acaoInimigo(){
+    function acaoInimigo() {
         let escolha = Math.floor(Math.random() * habilidadesVilao.length);
         let habilidade = habilidadesVilao[escolha]
         setInterval(alert(`O inimigo usou ${habilidade.nome}`), 2000)
         setVidaHeroi(vidaHeroi - habilidade.dano)
+        if (vidaHeroi - habilidade.dano <= 0) {
+            alert("Você perdeu!")
+            setVenceu(2)
+        }
     }
 
-    function acoesTurno(dano, mana){
-        if(manaHeroi - mana < 0){
+    function acoesTurno(dano, mana) {
+        if (manaHeroi - mana < 0) {
             alert("Não tem mana o suficiente")
         } else {
             acaoHeroi(dano, mana)
-            if(vidaInimigo - dano <= 0){
+            if (vidaInimigo - dano <= 0) {
                 alert("Você derrotou o " + nomeInimigo)
+                setVenceu(1)
             } else {
                 acaoInimigo()
             }
         }
     }
 
-    function novoTurno(){
-        setManaHeroi(manaHeroi + 1)
-    }
-
-    function turno(e){
+    function turno(e) {
         let dano = e.target.attributes[0].value
         let mana = e.target.attributes[1].value
         acoesTurno(dano, mana)
-        if(manaHeroi < 6){
-            novoTurno()
-        }
+    }
+
+    function tentarNovamente(){
+        document.location.reload()
     }
 
     return (
@@ -85,6 +101,17 @@ function Fase(props) {
                             </div>
                         </div>
                     </div>
+                    {venceu == 1 &&
+                    <div>
+                        Você venceu
+                        <Link to={props.proximaFase}>Próxima fase</Link>
+                    </div>}
+                    {venceu == 2 && 
+                    <div>
+                        Você perdeu
+                        <button onClick={tentarNovamente}>Tentar novamente</button>
+                        <button><Link to='/'>Voltar ao menu</Link></button>
+                    </div>}
                 </div>
             </div>
         </div>
